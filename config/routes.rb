20ac66ec285e06ef32271bd8266079c96c547c24
Rukaiya Amin <rukaiyaamin36@gmail.com>
2021-10-27 15:45:20 +0600

    commit 27

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'store#index', as: 'store_index'
+  
   resources :products
+  resources :carts
+  resources :store
+  resources :line
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
