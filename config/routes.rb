4f88d4fb6e93ea400b4858ead7af35d39c31d6f5
Rukaiya Amin <rukaiyaamin36@gmail.com>
2021-10-31 15:57:25 +0600

    Working on PayType

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :carts
   resources :line_items
 
-  # root 'store#index', as: 'store_index'
+  root 'store#index', as: 'store_index'
 
 
   resources :products do
